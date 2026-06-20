39f873a984e3c338a0f3cfb154ef01e456957377
CHatingPython <chatingpython@kpgpmc.org>
2026-06-20 12:26:53 +0200

    docs(furvm): document detail folder
    Refs: #12

--- furvm/include/furvm/detail/serialization.hpp
+++ furvm/include/furvm/detail/serialization.hpp
@@ -8,15 +8,69 @@
 namespace furvm {
 namespace detail {
 
+/**
+ * @brief Serializes an integer.
+ *
+ * @param os Output stream.
+ * @param value Integer.
+ * @return The output stream.
+ */
 std::ostream& serialize(std::ostream& os, std::int8_t value);
 std::ostream& serialize(std::ostream& os, std::int16_t value);
+
+/**
+ * @brief Serializes an integer.
+ *
+ * @param os Output stream.
+ * @param value Integer.
+ * @return The output stream.
+ */
 std::ostream& serialize(std::ostream& os, std::int32_t value);
 std::ostream& serialize(std::ostream& os, std::int64_t value);
+
+/**
+ * @brief Serializes an integer.
+ *
+ * @param os Output stream.
+ * @param value Integer.
+ * @return The output stream.
+ */
 std::ostream& serialize(std::ostream& os, std::uint8_t value);
+
+/**
+ * @brief Serializes an integer.
+ *
+ * @param os Output stream.
+ * @param value Integer.
+ * @return The output stream.
+ */
 std::ostream& serialize(std::ostream& os, std::uint16_t value);
+
+/**
+ * @brief Serializes an integer.
+ *
+ * @param os Output stream.
+ * @param value Integer.
+ * @return The output stream.
+ */
 std::ostream& serialize(std::ostream& os, std::uint32_t value);
+
+/**
+ * @brief Serializes an integer.
+ *
+ * @param os Output stream.
+ * @param value Integer.
+ * @return The output stream.
+ */
 std::ostream& serialize(std::ostream& os, std::uint64_t value);
 
+/**
+ * @brief Serializes a string.
+ *
+ * @param os Output stream.
+ * @param value String.
+ * @return The output stream.
+ */
 std::ostream& serialize(std::ostream& os, const std::string& value);
 
 } // namespace detail
